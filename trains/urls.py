a36633179e9071a2694675437271bdded13cdb88
nikita <nikit0zpart0z@gmail.com>
2021-06-20 23:39:20 +0300

    Added editing and deleting

--- trains/urls.py
+++ trains/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', ViewTrains.as_view(), name='view_trains'),
     path('add_train/', AddTrain.as_view(), name='add_train'),
     path('train/<int:train_pk>/', ViewTrain.as_view(), name='view_train'),
-    #    path('edit_city/<int:city_pk>/', EditCity.as_view(), name='edit_city'),
-    #    path('delete_city/<int:city_pk>/', DeleteCity.as_view(), name='delete_city'),
+    path('edit_train/<int:train_pk>/', EditTrain.as_view(), name='edit_train'),
+    path('delete_train/<int:train_pk>/', DeleteTrain.as_view(), name='delete_train'),
 ]
--- trains/views.py
+++ trains/views.py
@@ -17,12 +17,12 @@ class AddTrain(CreateView):
     template_name = 'trains/add_train.html'
     success_url = reverse_lazy('trains:add_train')
 
-    def is_valid(self, form):
+    def form_valid(self, form):
         form.save()
         messages.success(self.request, "Train successfully added")
         return super(AddTrain, self).form_valid(form)
 
-    def is_invalid(self, form):
+    def form_invalid(self, form):
         messages.error(self.request, "Error during adding train")
         return super(AddTrain, self).form_invalid(form)
 
@@ -32,3 +32,30 @@ class ViewTrain(DetailView):
     template_name = 'trains/view_train.html'
     context_object_name = 'train'
     pk_url_kwarg = 'train_pk'
+
+
+class EditTrain(UpdateView):
+    model = Train
+    form_class = AddTrainForm
+    template_name = 'trains/edit_train.html'
+    pk_url_kwarg = 'train_pk'
+    success_url = reverse_lazy('trains:view_trains')
+
+    def is_valid(self, form):
+        form.save()
+        messages.success(self.request, "Train successfully edited")
+        return super(EditTrain, self).form_valid(form)
+
+    def is_invalid(self, form):
+        messages.error(self.request, "Error during adding train")
+        return super(EditTrain, self).form_invalid(form)
+
+
+class DeleteTrain(DeleteView):
+    model = Train
+    pk_url_kwarg = 'train_pk'
+    success_url = reverse_lazy('trains:view_trains')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(self.request, "Train successfully deleted")
+        return self.post(request, *args, **kwargs)
