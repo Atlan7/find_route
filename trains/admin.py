1d9c5ac72055556dd1cbc90e5356f52afd657a0c
nikita <nikit0zpart0z@gmail.com>
2021-06-20 22:56:28 +0300

    Edited admin panel

--- trains/admin.py
+++ trains/admin.py
@@ -6,9 +6,9 @@ from trains.models import Train
 class TrainAdmin(admin.ModelAdmin):
     class Meta:
         model = Train
+    list_display = ('train_number', 'from_city', 'to_city', 'trip_start_time', 'trip_end_time',)
 
 
-
-admin.site.register(Train)
+admin.site.register(Train, TrainAdmin)
 
 
