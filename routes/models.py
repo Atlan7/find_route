9f65a197f6444f8a5064305923a34a8e44125ed3
nikita <nikit0zpart0z@gmail.com>
2021-06-22 00:32:03 +0300

    Created model for routes

--- routes/models.py
+++ routes/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from cities.models import City
+from trains.models import Train
+
+
+class Route(models.Model):
+    route_name = models.CharField(max_length=100, unique=True, verbose_name='Route name')
+    from_city = models.ForeignKey(
+        City, on_delete=models.CASCADE, related_name='route_from_city_set', verbose_name='from city'
+    )
+    to_city = models.ForeignKey(
+        City, on_delete=models.CASCADE, related_name='route_to_city_set', verbose_name='to city'
+    )
+    trip_start_time = models.DateTimeField()
+    trip_end_time = models.DateTimeField()
+    trains = models.ManyToManyField(Train, verbose_name='List of trains')
+
+    def __str__(self):
+        return f'Route: {self.route_name}, '\
+               f'goes from: {self.from_city} ' \
+               f'to: {self.to_city}, ' \
+               f'departs at: {self.trip_start_time}, ' \
+               f'arrives at: {self.trip_end_time}, ' \
+               f'total trip time: {self.trip_end_time - self.trip_start_time} '
+
+    class Meta:
+        verbose_name = 'Route'
+        verbose_name_plural = 'Routes'
+        ordering = ['route_name']
