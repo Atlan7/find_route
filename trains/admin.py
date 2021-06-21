faf58366fd298003fe3d52e94056a4269b546e55
nikita <nikit0zpart0z@gmail.com>
2021-06-22 00:09:27 +0300

    Added total trip time at list display

--- trains/admin.py
+++ trains/admin.py
@@ -3,12 +3,9 @@ from django.contrib import admin
 from trains.models import Train
 
 
+@admin.register(Train)
 class TrainAdmin(admin.ModelAdmin):
-    class Meta:
-        model = Train
-    list_display = ('train_number', 'from_city', 'to_city', 'trip_start_time', 'trip_end_time',)
+    list_display = ('train_number', 'from_city', 'to_city', 'trip_start_time', 'trip_end_time', 'total_trip_time',)
 
-
-admin.site.register(Train, TrainAdmin)
 
 
