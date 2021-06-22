aef716ba7844e677b9b671710b932fdbb8cf6229
nikita <nikit0zpart0z@gmail.com>
2021-06-22 18:18:31 +0300

    Views for find route

--- routes/views.py
+++ routes/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.views.generic import FormView
 
-# Create your views here.
+
+from .models import Route
+from .forms import FindRouteForm
+
+
+class FindRoute(FormView):
+    form_class = FindRouteForm
+    template_name = 'routes/find_route.html'
+
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request, 'Success')
+        return super(FindRoute, self).form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'No data for searching')
+        return super(FindRoute, self).form_invalid(form)
