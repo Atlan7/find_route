857ab34d343b9f5efa5f44c591e365bdf67fea0e
nikita <nikit0zpart0z@gmail.com>
2021-07-09 00:36:44 +0300

    Added to view trains from/to city at view city page

--- cities/urls.py
+++ cities/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('city/<int:city_pk>/', ViewCity.as_view(), name='view_city'),
     path('edit_city/<int:city_pk>/', EditCity.as_view(), name='edit_city'),
     path('delete_city/<int:city_pk>/', DeleteCity.as_view(), name='delete_city'),
+    path('trains_from_city/<int:from_city_pk>/', ViewTrainsFromCity.as_view(), name='trains_from_city'),
+    path('trains_to_city/<int:to_city_pk>/', ViewTrainsToCity.as_view(), name='trains_to_city'),
 ]
